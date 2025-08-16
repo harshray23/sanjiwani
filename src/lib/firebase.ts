4cae1e7cb6da259259c59fb63bdf948be046cddb
laptop <laptopharsh0108@gmail.com>
2025-08-16 13:16:02 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,18 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration - using the one originally fetched.
+// Your web app's Firebase configuration - using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyASaPbn_C9TY0mwjCtQZJt6aOAxLNGKdHA",
-  authDomain: "hospconnect-drjo0.firebaseapp.com",
-  projectId: "hospconnect-drjo0",
-  storageBucket: "hospconnect-drjo0.appspot.com",
-  messagingSenderId: "203104311053",
-  appId: "1:203104311053:web:99bc2430f9bb40c2068244"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
 
 // Initialize Firebase
 let app;
